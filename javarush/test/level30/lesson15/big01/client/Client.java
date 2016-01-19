bb5d4c40396bceb84c56531698ac78382c32a8dd
shds <dmitryshds@ukr.net>
2016-01-19 21:27:34 +0200

    30_15_big_14

--- javarush/test/level30/lesson15/big01/client/Client.java
+++ javarush/test/level30/lesson15/big01/client/Client.java
@@ -1,6 +1,11 @@
 package com.javarush.test.level30.lesson15.big01.client;
 
 import com.javarush.test.level30.lesson15.big01.Connection;
+import com.javarush.test.level30.lesson15.big01.ConsoleHelper;
+import com.javarush.test.level30.lesson15.big01.Message;
+import com.javarush.test.level30.lesson15.big01.MessageType;
+
+import java.io.IOException;
 
 /**
  * Created by Dmitriy on 19.01.2016.
@@ -9,8 +14,42 @@ public class Client {
     protected Connection connection;
     private volatile boolean clientConnected = false;
 
-    public class SocketThread extends Thread{
 
+    protected String getServerAddress() {
+
+        ConsoleHelper.writeMessage("Enter server address");
+        return ConsoleHelper.readString();
+    }
+
+    protected int getServerPort() {
+        ConsoleHelper.writeMessage("Enter server port");
+        return ConsoleHelper.readInt();
+    }
+
+    protected String getUserName() {
+        ConsoleHelper.writeMessage("Enter user name");
+        return ConsoleHelper.readString();
+    }
+
+    protected boolean shouldSentTextFromConsole() {
+        return true;
+    }
+
+    protected SocketThread getSocketThread() {
+        return new SocketThread();
+    }
+
+    protected void sendTextMessage(String text) {
+
+        try {
+            connection.send(new Message(MessageType.TEXT,text));
+        } catch (IOException e) {
+            ConsoleHelper.writeMessage("Exception send Text Message");
+            clientConnected = false;
+        }
+    }
+
+    public class SocketThread extends Thread {
 
 
     }

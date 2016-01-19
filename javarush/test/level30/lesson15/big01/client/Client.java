50597dc820f3007c5077331428aac265e6a523fa
shds <dmitryshds@ukr.net>
2016-01-19 20:54:59 +0200

    30_15_big_13

---
+++ javarush/test/level30/lesson15/big01/client/Client.java
@@ -0,0 +1,17 @@
+package com.javarush.test.level30.lesson15.big01.client;
+
+import com.javarush.test.level30.lesson15.big01.Connection;
+
+/**
+ * Created by Dmitriy on 19.01.2016.
+ */
+public class Client {
+    protected Connection connection;
+    private volatile boolean clientConnected = false;
+
+    public class SocketThread extends Thread{
+
+
+
+    }
+}

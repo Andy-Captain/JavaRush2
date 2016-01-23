60cb8e00ed916eb590ebba2979c243e479f18b6f
shds <dmitryshds@ukr.net>
2016-01-23 23:19:47 +0200

    31_15_big_8

--- javarush/test/level31/lesson15/big01/CommandExecutor.java
+++ javarush/test/level31/lesson15/big01/CommandExecutor.java
@@ -5,29 +5,22 @@ import com.javarush.test.level31.lesson15.big01.command.*;
 import java.util.HashMap;
 import java.util.Map;
 
-/**
- * Created by Dmitriy on 23.01.2016.
- */
 public class CommandExecutor {
-
-
     private static final Map<Operation, Command> allKnownCommandsMap = new HashMap<>();
+
     static {
-              allKnownCommandsMap.put(Operation.ADD,new ZipAddCommand());
-              allKnownCommandsMap.put(Operation.CONTENT,new ZipContentCommand());
-              allKnownCommandsMap.put(Operation.CREATE,new ZipCreateCommand());
-              allKnownCommandsMap.put(Operation.EXTRACT,new ZipExtractCommand());
-              allKnownCommandsMap.put(Operation.REMOVE,new ZipRemoveCommand());
-              allKnownCommandsMap.put(Operation.EXIT,new ExitCommand());
-           }
-    private CommandExecutor(){}
+        allKnownCommandsMap.put(Operation.CREATE, new ZipCreateCommand());
+        allKnownCommandsMap.put(Operation.ADD, new ZipAddCommand());
+        allKnownCommandsMap.put(Operation.REMOVE, new ZipRemoveCommand());
+        allKnownCommandsMap.put(Operation.EXTRACT, new ZipExtractCommand());
+        allKnownCommandsMap.put(Operation.CONTENT, new ZipContentCommand());
+        allKnownCommandsMap.put(Operation.EXIT, new ExitCommand());
+    }
 
-    public static void execute(Operation operation){
-        try {
-           allKnownCommandsMap.get(operation).execute();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+    private CommandExecutor() {
+    }
 
+    public static void execute(Operation operation) throws Exception {
+        allKnownCommandsMap.get(operation).execute();
     }
 }

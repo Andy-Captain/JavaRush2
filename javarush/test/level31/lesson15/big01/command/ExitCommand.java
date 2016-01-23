3d0e843022a6261beffd03320d417781ea1e4958
shds <dmitryshds@ukr.net>
2016-01-23 22:26:51 +0200

    31_15_big_6

--- javarush/test/level31/lesson15/big01/command/ExitCommand.java
+++ javarush/test/level31/lesson15/big01/command/ExitCommand.java
@@ -2,9 +2,6 @@ package com.javarush.test.level31.lesson15.big01.command;
 
 import com.javarush.test.level31.lesson15.big01.ConsoleHelper;
 
-/**
- * Created by Dmitriy on 23.01.2016.
- */
 public class ExitCommand implements Command {
     @Override
     public void execute() throws Exception {

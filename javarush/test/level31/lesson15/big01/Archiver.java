0fdbdf848a46e56eae6fc4ef9d0fa370a612c89d
shds <dmitryshds@ukr.net>
2016-01-24 00:07:54 +0200

    31_15_big_9

--- javarush/test/level31/lesson15/big01/Archiver.java
+++ javarush/test/level31/lesson15/big01/Archiver.java
@@ -1,28 +1,45 @@
 package com.javarush.test.level31.lesson15.big01;
 
-import com.javarush.test.level31.lesson15.big01.command.ExitCommand;
+import com.javarush.test.level31.lesson15.big01.exception.WrongZipFileException;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.nio.file.Paths;
+import java.io.IOException;
 
 /**
  * Created by Dmitriy on 23.01.2016.
  */
 public class Archiver {
 
-    public static void main(String[] args)  {
+    public static void main(String[] args) {
         try {
-            BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-            System.out.println("Enter full archive path and name >> example: D:/temp/abc.zip");
-            ZipFileManager zipFileManager = new ZipFileManager(Paths.get(reader.readLine()));
-            System.out.println("Enter full FILE path and name >> example: D:/temp/a.txt");
-            zipFileManager.createZip(Paths.get(reader.readLine()));
+            Operation operation = null;
+
+            while (!(operation == Operation.EXIT)) {
+
+                try {
+                        operation = askOperation();
+                        CommandExecutor.execute(operation);
+                } catch (WrongZipFileException e) {
+                    ConsoleHelper.writeMessage("Вы не выбрали файл архива или выбрали неверный файл.");
+                } catch (Throwable e) {
+                    ConsoleHelper.writeMessage("Произошла ошибка. Проверьте введенные данные.");
+                }
+            }
 
 
-            new ExitCommand().execute();
+
         } catch (Exception e) {
             e.printStackTrace();
         }
+    }
+
+    public static Operation askOperation() throws IOException {
+        ConsoleHelper.writeMessage("Выберите операцию:");
+        ConsoleHelper.writeMessage(" " + Operation.CREATE.ordinal() + " - упаковать файлы в архив");
+        ConsoleHelper.writeMessage(" " + Operation.ADD.ordinal() + " - добавить файл в архив");
+        ConsoleHelper.writeMessage(" " + Operation.REMOVE.ordinal() + " - удалить файл из архива");
+        ConsoleHelper.writeMessage(" " + Operation.EXTRACT.ordinal() + " - распаковать архив");
+        ConsoleHelper.writeMessage(" " + Operation.CONTENT.ordinal() + " - просмотреть содержимое архива");
+        ConsoleHelper.writeMessage(" " + Operation.EXIT.ordinal() + " - выход");
+        return Operation.values()[ConsoleHelper.readInt()];
     }
 }
--- javarush/test/level31/lesson15/big01/exception/PathIsNotFoundException.java
+++ javarush/test/level31/lesson15/big01/exception/PathIsNotFoundException.java
@@ -1,7 +1,4 @@
 package com.javarush.test.level31.lesson15.big01.exception;
 
-/**
- * Created by Dmitriy on 23.01.2016.
- */
 public class PathIsNotFoundException extends Exception {
 }

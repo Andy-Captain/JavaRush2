27f50830513d4d458b9c99499cd32b0cb130a359
shds <dmitryshds@ukr.net>
2016-01-24 19:59:47 +0200

    31_15_big_13

--- javarush/test/level31/lesson15/big01/command/ZipCommand.java
+++ javarush/test/level31/lesson15/big01/command/ZipCommand.java
@@ -3,16 +3,14 @@ package com.javarush.test.level31.lesson15.big01.command;
 import com.javarush.test.level31.lesson15.big01.ConsoleHelper;
 import com.javarush.test.level31.lesson15.big01.ZipFileManager;
 
-import java.nio.file.*;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
-public abstract class ZipCommand implements Command{
-
+public abstract class ZipCommand implements Command {
 
     public ZipFileManager getZipFileManager() throws Exception{
-        ConsoleHelper.writeMessage("Enter full path to arhive example: D:/temp/abc.zip");
-        String zipPath = ConsoleHelper.readString();
-        Path pathToZip = Paths.get(zipPath);
-       return new ZipFileManager(pathToZip);
-
+        ConsoleHelper.writeMessage("Введите полный путь файла архива:");
+        Path zipPath = Paths.get(ConsoleHelper.readString());
+        return new ZipFileManager(zipPath);
     }
 }
--- javarush/test/level31/lesson15/big01/command/ZipCreateCommand.java
+++ javarush/test/level31/lesson15/big01/command/ZipCreateCommand.java
@@ -4,24 +4,25 @@ import com.javarush.test.level31.lesson15.big01.ConsoleHelper;
 import com.javarush.test.level31.lesson15.big01.ZipFileManager;
 import com.javarush.test.level31.lesson15.big01.exception.PathIsNotFoundException;
 
-import java.nio.file.*;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 public class ZipCreateCommand extends ZipCommand {
     @Override
     public void execute() throws Exception {
         try {
             ConsoleHelper.writeMessage("Создание архива.");
+
             ZipFileManager zipFileManager = getZipFileManager();
-            ConsoleHelper.writeMessage("Enter full path to file or directory example: D:/temp/a.txt");
-            String filePath = ConsoleHelper.readString();
-            Path pathToFile = Paths.get(filePath);
-            zipFileManager.createZip(pathToFile);
+
+            ConsoleHelper.writeMessage("Введите полное имя файла или директории для архивации:");
+            Path sourcePath = Paths.get(ConsoleHelper.readString());
+            zipFileManager.createZip(sourcePath);
+
             ConsoleHelper.writeMessage("Архив создан.");
-        }catch (PathIsNotFoundException e)
-        {
+
+        } catch (PathIsNotFoundException e) {
             ConsoleHelper.writeMessage("Вы неверно указали имя файла или директории.");
-
         }
-
     }
 }

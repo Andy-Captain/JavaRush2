b4e41bb6d635c62cceab08465f217711967d91f9
shds <dmitryshds@ukr.net>
2016-01-25 12:38:03 +0200

    31_15_big_14

--- javarush/test/level31/lesson15/big01/FileProperties.java
+++ javarush/test/level31/lesson15/big01/FileProperties.java
@@ -1,7 +1,6 @@
 package com.javarush.test.level31.lesson15.big01;
 
 public class FileProperties {
-
     private String name;
     private long size;
     private long compressedSize;
@@ -14,47 +13,43 @@ public class FileProperties {
         this.compressionMethod = compressionMethod;
     }
 
-    public long getCompressionRatio() {
-
-        return (100 - ((compressedSize * 100) / size));
-    }
-
-    @Override
-    public String toString() {
-        return (size > 0) ?
-                String.format("%s %f Kb (%f Kb) сжатие: %d%%", name, (float)(size / 1024) , (float)(compressedSize / 1024) , getCompressionRatio())
-                : name;
-    }
 
     public String getName() {
         return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
     }
 
     public long getSize() {
         return size;
     }
 
-    public void setSize(long size) {
-        this.size = size;
-    }
-
     public long getCompressedSize() {
         return compressedSize;
-    }
-
-    public void setCompressedSize(long compressedSize) {
-        this.compressedSize = compressedSize;
     }
 
     public int getCompressionMethod() {
         return compressionMethod;
     }
 
-    public void setCompressionMethod(int compressionMethod) {
-        this.compressionMethod = compressionMethod;
+    public long getCompressionRatio() {
+        // Вычисляем степень сжатия
+        return 100 - ((compressedSize * 100) / size);
+    }
+
+    @Override
+    public String toString() {
+        // Строим красивую строку из свойств
+        StringBuilder builder = new StringBuilder();
+        builder.append(name);
+        if (size > 0) {
+            builder.append("\t");
+            builder.append(size / 1024);
+            builder.append(" Kb (");
+            builder.append(compressedSize / 1024);
+            builder.append(" Kb) сжатие: ");
+            builder.append(getCompressionRatio());
+            builder.append("%");
+        }
+
+        return builder.toString();
     }
 }

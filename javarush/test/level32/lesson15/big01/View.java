9445985958518e3f5e4c704600f391402659418d
shds <dmitryshds@ukr.net>
2016-01-25 22:27:31 +0200

    32_15_big_7

--- javarush/test/level32/lesson15/big01/View.java
+++ javarush/test/level32/lesson15/big01/View.java
@@ -1,8 +1,10 @@
 package com.javarush.test.level32.lesson15.big01;
 
 import com.javarush.test.level32.lesson15.big01.listeners.FrameListener;
+import com.javarush.test.level32.lesson15.big01.listeners.TabbedPaneChangeListener;
 
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
@@ -17,14 +19,16 @@ public class View extends JFrame implements ActionListener {
     public void actionPerformed(ActionEvent e) {
 
     }
-    public void init(){
 
-      initGui();
+    public void init() {
+
+        initGui();
         FrameListener frameListener = new FrameListener(this);
         this.addWindowListener(frameListener);
         this.setVisible(true);
 
     }
+
     public Controller getController() {
         return controller;
     }
@@ -33,17 +37,27 @@ public class View extends JFrame implements ActionListener {
         this.controller = controller;
     }
 
-    public void exit(){
+    public void exit() {
         controller.exit();
     }
-    public void initMenuBar(){
+
+    public void initMenuBar() {
 
     }
-    public void initEditor(){
 
+    public void initEditor() {
+        htmlTextPane.setContentType("text/html");
+        JScrollPane jScrollPane = new JScrollPane(htmlTextPane);
+        tabbedPane.addTab("HTML", jScrollPane);
+
+        JScrollPane jScrollPane1 = new JScrollPane(plainTextPane);
+        tabbedPane.addTab("Текст", jScrollPane1);
+        tabbedPane.setPreferredSize(new Dimension(500, 500));
+        tabbedPane.addChangeListener(new TabbedPaneChangeListener(this));
+        getContentPane().add(tabbedPane,BorderLayout.CENTER);
     }
 
-    public void initGui(){
+    public void initGui() {
         initMenuBar();
         initEditor();
         pack();

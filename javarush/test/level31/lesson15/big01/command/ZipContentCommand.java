3830c607702dc11ef12b67a816d1732a7310aa49
shds <dmitryshds@ukr.net>
2016-01-25 12:49:43 +0200

    31_15_big_15

--- javarush/test/level31/lesson15/big01/command/ZipContentCommand.java
+++ javarush/test/level31/lesson15/big01/command/ZipContentCommand.java
@@ -1,8 +1,24 @@
 package com.javarush.test.level31.lesson15.big01.command;
 
+import com.javarush.test.level31.lesson15.big01.ConsoleHelper;
+import com.javarush.test.level31.lesson15.big01.FileProperties;
+import com.javarush.test.level31.lesson15.big01.ZipFileManager;
+
+import java.util.List;
+
 public class ZipContentCommand extends ZipCommand {
     @Override
     public void execute() throws Exception {
+        ConsoleHelper.writeMessage("Просмотр содержимого архива.");
+        ZipFileManager zipFileManager = getZipFileManager();
+        ConsoleHelper.writeMessage("Содержимое архива:");
+        List<FileProperties> filesList = zipFileManager.getFilesList();
+        for (FileProperties properties : filesList) {
+            ConsoleHelper.writeMessage(properties.toString());
+            ConsoleHelper.writeMessage("Содержимое архива прочитано.");
+
+        }
+
 
     }
 }

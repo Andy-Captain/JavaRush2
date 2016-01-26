c4d9df907efb7319c47860e275cedeb7b8d77993
shds <dmitryshds@ukr.net>
2016-01-26 22:30:10 +0200

    33_15_big_4

--- javarush/test/level33/lesson15/big01/Shortener.java
+++ javarush/test/level33/lesson15/big01/Shortener.java
@@ -1,14 +1,30 @@
 package com.javarush.test.level33.lesson15.big01;
 
 
+import com.javarush.test.level33.lesson15.big01.strategies.StorageStrategy;
 
 public class Shortener {
+    private Long lastId = 0L;
+    private StorageStrategy storageStrategy;
 
-
-    public Long getId(String string){
-        return null;
+    public Shortener(StorageStrategy storageStrategy) {
+        this.storageStrategy = storageStrategy;
     }
-    public String getString(Long id){
-        return null;
+
+    public synchronized Long getId(String string) {
+        if (storageStrategy.containsValue(string)) {
+            return storageStrategy.getKey(string);
+        } else {
+            lastId++;
+            storageStrategy.put(lastId, string);
+            return lastId;
+        }
+
+
+    }
+
+    public synchronized String getString(Long id) {
+
+        return storageStrategy.getValue(id);
     }
 }

2d7966a169b19412ba860be9e2e499fb94de4c9c
shds <dmitryshds@ukr.net>
2016-01-26 20:45:57 +0200

    32_15_big_22

---
+++ javarush/test/level32/lesson15/big01/HTMLFileFilter.java
@@ -0,0 +1,22 @@
+package com.javarush.test.level32.lesson15.big01;
+
+import javax.swing.filechooser.FileFilter;
+import java.io.File;
+
+public class HTMLFileFilter extends FileFilter {
+
+
+    @Override
+    public boolean accept(File f) {
+        if (f.getName().toLowerCase().endsWith(".html") || f.getName().toLowerCase().endsWith(".htm")|| f.isDirectory()) {
+            return true;
+        }
+
+        return false;
+    }
+
+    @Override
+    public String getDescription() {
+        return "HTML и HTM файлы";
+    }
+}

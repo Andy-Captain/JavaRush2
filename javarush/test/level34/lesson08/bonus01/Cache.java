7a2fdd9df60ceef1c2fb9fe799b8cb034bbc20af
shds <dmitryshds@ukr.net>
2016-01-28 23:20:35 +0200

    ALL 28_01_16

--- javarush/test/level34/lesson08/bonus01/Cache.java
+++ javarush/test/level34/lesson08/bonus01/Cache.java
@@ -1,17 +1,39 @@
 package com.javarush.test.level34.lesson08.bonus01;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.util.Map;
+import java.util.WeakHashMap;
 
 public class Cache<K, V> {
-    private Map<K, V> cache = null;   //TODO add your code here
+    private Map<K , V> cache =  new WeakHashMap <>();   //TODO add your code here
 
     public V getByKey(K key, Class<V> clazz) throws Exception {
-        //TODO add your code here
-        return null;
+
+        if(!cache.containsKey(key)){
+
+
+            cache.put(key, clazz.getConstructor(key.getClass()).newInstance(key) );
+        }
+        return cache.get(key);
+
+
+
+
     }
 
     public boolean put(V obj) {
-        //TODO add your code here
+        try {
+            Method getKey = obj.getClass().getDeclaredMethod("getKey");
+            getKey.setAccessible(true);
+            cache.put((K) getKey.invoke(obj),obj);
+
+            return true;
+        } catch (IllegalAccessException | InvocationTargetException | NoSuchMethodException e) {
+            e.printStackTrace();
+        }
+
+
         return false;
     }
 

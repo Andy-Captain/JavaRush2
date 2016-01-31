42cff5ba74254c9aeb70cb84432f55bf2337ccc9
shds <dmitryshds@ukr.net>
2016-01-31 14:03:25 +0200

    34_15_big_10

--- javarush/test/level34/lesson15/big01/view/Field.java
+++ javarush/test/level34/lesson15/big01/view/Field.java
@@ -1,13 +1,20 @@
 package com.javarush.test.level34.lesson15.big01.view;
 
+import com.javarush.test.level34.lesson15.big01.controller.EventListener;
+
 import javax.swing.*;
 import java.awt.*;
 
 public class Field extends JPanel{
     private View view;
+    private EventListener eventListener;
 
     public Field(View view) {
         this.view = view;
+    }
+
+    public void setEventListener(EventListener eventListener) {
+        this.eventListener = eventListener;
     }
 
     public void paint(Graphics g){

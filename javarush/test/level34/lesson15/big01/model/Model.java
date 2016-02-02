c676825326dd3c435c7425b3191a5d61ff86a45d
shds <dmitryshds@ukr.net>
2016-02-02 21:19:34 +0200

    34_15_big_16

--- javarush/test/level34/lesson15/big01/model/Model.java
+++ javarush/test/level34/lesson15/big01/model/Model.java
@@ -3,6 +3,7 @@ package com.javarush.test.level34.lesson15.big01.model;
 import com.javarush.test.level34.lesson15.big01.controller.EventListener;
 
 import java.nio.file.Paths;
+import java.util.Set;
 
 public class Model {
     public static final int FIELD_SELL_SIZE = 20;
@@ -13,25 +14,120 @@ public class Model {
             (Paths.get("C:/JavaRushHomeWork/src/com/javarush/test/level34/lesson15/big01/res/levels.txt"));
 
 
-    public  GameObjects getGameObjects(){
+    public GameObjects getGameObjects() {
         return gameObjects;
     }
-    public void restartLevel(int level){
+
+    public void restartLevel(int level) {
         gameObjects = levelLoader.getLevel(level);
     }
 
-    public void restart(){
+    public void restart() {
         restartLevel(currentLevel);
     }
-    public void startNextLevel()
-    {
+
+    public void startNextLevel() {
         currentLevel++;
         restart();
     }
+
     public void setEventListener(EventListener eventListener) {
         this.eventListener = eventListener;
     }
 
     public void move(Direction direction) {
+        Player player = gameObjects.getPlayer();
+        if (checkWallCollision(player, direction)) {
+            return;
+        }
+        if (checkBoxCollision(direction)) {
+            return;
+        }
+        int count = FIELD_SELL_SIZE;
+        switch (direction){
+            case LEFT:
+                player.move(-count,0);
+                break;
+            case RIGHT:
+                player.move(count,0);
+                break;
+            case UP:
+                player.move(0,-count);
+                break;
+            case DOWN:
+                player.move(0,count);
+        }
+        checkCompletion();
     }
+
+    public boolean checkWallCollision(CollisionObject gameObject, Direction direction) {
+
+        Set<Wall> walls = gameObjects.getWalls();
+        for (Wall wall : walls) {
+            if (gameObject.isCollision(wall, direction)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public boolean checkBoxCollision(Direction direction) {
+        Player player = gameObjects.getPlayer();
+
+        GameObject stoped = null;
+        for (GameObject gameObject : gameObjects.getAll()) {
+            if (!(gameObject instanceof Player) && !(gameObject instanceof Home) && player.isCollision(gameObject, direction)) {
+                stoped = gameObject;
+            }
+        }
+
+        if ((stoped == null)) {
+            return false;
+        }
+        if (stoped instanceof Box) {
+            Box stopedBox = (Box) stoped;
+            if (checkWallCollision(stopedBox, direction)) {
+                return true;
+            }
+            for (Box box : gameObjects.getBoxes()) {
+                if (stopedBox.isCollision(box, direction)) {
+                    return true;
+                }
+            }
+            switch (direction) {
+                case LEFT:
+                    stopedBox.move(-FIELD_SELL_SIZE, 0);
+                    break;
+                case RIGHT:
+                    stopedBox.move(FIELD_SELL_SIZE, 0);
+                    break;
+                case UP:
+                    stopedBox.move(0, -FIELD_SELL_SIZE);
+                    break;
+                case DOWN:
+                    stopedBox.move(0, FIELD_SELL_SIZE);
+            }
+        }
+        return false;
+    }
+
+    public void checkCompletion() {
+        Set<Home> homes = gameObjects.getHomes();
+        Set<Box> boxes = gameObjects.getBoxes();
+        int count = 0;
+        for (Home home : homes) {
+
+            for (Box box : boxes) {
+
+                if (home.getX() == box.getX() && home.getY() == box.getY()) {
+                    count++;
+                }
+            }
+        }
+        if (count == homes.size()) {
+            eventListener.levelCompleted(currentLevel);
+        }
+    }
+
+
 }

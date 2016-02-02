1dccb3d6d3485c98ceee97762ce889a06ce8a438
shds <dmitryshds@ukr.net>
2016-02-02 20:00:47 +0200

    34_15_big_15

--- javarush/test/level34/lesson15/big01/view/Field.java
+++ javarush/test/level34/lesson15/big01/view/Field.java
@@ -1,11 +1,14 @@
 package com.javarush.test.level34.lesson15.big01.view;
 
 import com.javarush.test.level34.lesson15.big01.controller.EventListener;
+import com.javarush.test.level34.lesson15.big01.model.Direction;
 import com.javarush.test.level34.lesson15.big01.model.GameObject;
 import com.javarush.test.level34.lesson15.big01.model.GameObjects;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 import java.util.Set;
 
 public class Field extends JPanel {
@@ -14,6 +17,9 @@ public class Field extends JPanel {
 
     public Field(View view) {
         this.view = view;
+        KeyHandler keyHandler = new KeyHandler(this);
+        this.addKeyListener(keyHandler);
+        this.setFocusable(true);
     }
 
     public void setEventListener(EventListener eventListener) {
@@ -29,5 +35,43 @@ public class Field extends JPanel {
             gameObject.draw(g);
         }
 
+    }
+
+    public static class KeyHandler extends KeyAdapter {
+        private Field field;
+
+        public KeyHandler(Field field) {
+            this.field = field;
+        }
+
+        @Override
+        public void keyPressed(KeyEvent e) {
+
+            switch (e.getKeyCode()) {
+                case KeyEvent.VK_LEFT:
+                    field.eventListener.move(Direction.LEFT);
+                    break;
+
+                case KeyEvent.VK_RIGHT:
+                    field.eventListener.move(Direction.RIGHT);
+                    break;
+
+                case KeyEvent.VK_UP:
+                    field.eventListener.move(Direction.UP);
+                    break;
+
+                case KeyEvent.VK_DOWN:
+                    field.eventListener.move(Direction.DOWN);
+                    break;
+
+                case KeyEvent.VK_R:
+                    field.eventListener.restart();
+                    break;
+
+
+            }
+
+
+        }
     }
 }

c223060019d4d29d373946d5a8f06cbfd96f81f9
shds <dmitryshds@ukr.net>
2016-02-11 16:38:52 +0200

    36_04_big_05

---
+++ javarush/test/level36/lesson04/big01/model/MainModel.java
@@ -0,0 +1,24 @@
+package com.javarush.test.level36.lesson04.big01.model;
+
+import com.javarush.test.level36.lesson04.big01.bean.User;
+import com.javarush.test.level36.lesson04.big01.model.service.UserService;
+import com.javarush.test.level36.lesson04.big01.model.service.UserServiceImpl;
+
+import java.util.List;
+
+public class MainModel implements Model{
+
+    private UserService userService = new UserServiceImpl();
+    private ModelData modelData = new ModelData();
+    @Override
+    public ModelData getModelData() {
+        return modelData;
+    }
+
+    @Override
+    public void loadUsers() {
+        List<User> usersBetweenLevels = userService.getUsersBetweenLevels(1, 100);
+        modelData.setUsers(usersBetweenLevels);
+
+    }
+}

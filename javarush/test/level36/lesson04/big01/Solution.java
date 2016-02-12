dea0ea658ca6d1ecc96ee445ddd8ca52aa75fc84
shds <dmitryshds@ukr.net>
2016-02-12 17:29:32 +0200

    36_04_big_08

--- javarush/test/level36/lesson04/big01/Solution.java
+++ javarush/test/level36/lesson04/big01/Solution.java
@@ -3,6 +3,7 @@ package com.javarush.test.level36.lesson04.big01;
 import com.javarush.test.level36.lesson04.big01.controller.Controller;
 import com.javarush.test.level36.lesson04.big01.model.MainModel;
 import com.javarush.test.level36.lesson04.big01.model.Model;
+import com.javarush.test.level36.lesson04.big01.view.EditUserView;
 import com.javarush.test.level36.lesson04.big01.view.UsersView;
 
 public class Solution {
@@ -11,12 +12,16 @@ public class Solution {
         Model model = new MainModel();
         UsersView usersView = new UsersView();
         Controller controller = new Controller();
-
+        EditUserView editUserView = new EditUserView();
         usersView.setController(controller);
 
         controller.setModel(model);
         controller.setUsersView(usersView);
+        controller.setEditUserView(editUserView);
+
         usersView.fireEventShowAllUsers();
         usersView.fireEventShowDeletedUsers();
+        usersView.fireEventOpenUserEditForm(126);
+
     }
 }

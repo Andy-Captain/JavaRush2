f3cbf7a494d92a9767db068c845e73a4b539ffcf
shds <dmitryshds@ukr.net>
2016-02-15 15:51:23 +0200

    36_10_bonus01_solv

--- javarush/test/level36/lesson10/bonus01/Solution.java
+++ javarush/test/level36/lesson10/bonus01/Solution.java
@@ -1,5 +1,11 @@
 package com.javarush.test.level36.lesson10.bonus01;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -15,12 +21,14 @@ import java.util.List;
 public class Solution {
     private List<Class> hiddenClasses = new ArrayList<>();
     private String packageName;
+
     public Solution(String packageName) {
         this.packageName = packageName;
     }
 
     public static void main(String[] args) throws ClassNotFoundException {
-        Solution solution = new Solution("C:\\JavaRushHomeWork\\src\\com\\javarush\\test\\level36\\lesson10\\bonus01\\data\\second");
+//        Solution solution = new Solution("C:\\JavaRushHomeWork\\src\\com\\javarush\\test\\level36\\lesson10\\bonus01\\data\\second");
+        Solution solution = new Solution("C:\\JavaRushHomeWork\\out\\production\\JavaRushHomeWork\\com\\javarush\\test\\level36\\lesson10\\bonus01\\data\\second");
         solution.scanFileSystem();
         System.out.println(solution.getHiddenClassObjectByKey("hiddenclassimplse"));
         System.out.println(solution.getHiddenClassObjectByKey("hiddenclassimplf"));
@@ -28,11 +36,81 @@ public class Solution {
     }
 
     public void scanFileSystem() throws ClassNotFoundException {
+        File dir = new File(packageName);
+        final String finalPath = dir.getAbsolutePath() + File.separator;
 
+        String[] classFiles = dir.list();
+        for (String classFile : classFiles) {
+            ClassLoader loader = new ClassLoader() {
+                @Override
+                protected Class<?> findClass(String name) throws ClassNotFoundException {
+                    try {
+                        byte[] temp = getBytesFrFile(finalPath + name + ".class");
+                        return defineClass(null, temp, 0, temp.length);
+                    } catch (IOException e) {
 
+                        throw new ClassNotFoundException();
+                    }
+                }
+            };
+            String className = classFile.substring(0, classFile.length() - 6);
+            try {
+                Class clazz = loader.loadClass(className);
+
+                Class[] interfaces = clazz.getInterfaces();
+
+                for (Class anInterface : interfaces) {
+
+                    if (anInterface.getSimpleName().equals("HiddenClass")) {
+                        hiddenClasses.add(clazz);
+                    }
+                }
+
+            } catch (ClassNotFoundException e) {
+                throw new ClassNotFoundException();
+
+            }
+        }
     }
 
     public HiddenClass getHiddenClassObjectByKey(String key) {
+        for (Class clazz : hiddenClasses) {
+            if (clazz.getSimpleName().toLowerCase().startsWith(key.toLowerCase())) {
+                try {
+                    Constructor[] constructors = clazz.getDeclaredConstructors();
+                    constructors[0].setAccessible(true);
+                    return (HiddenClass) constructors[0].newInstance(null);
+                } catch (InstantiationException | IllegalAccessException | InvocationTargetException e) {
+
+                }
+            }
+        }
         return null;
+    }
+
+    public static byte[] getBytesFrFile(String path) throws IOException {
+        File file = new File(path);
+        InputStream is = new FileInputStream(file);
+
+        long length = file.length();
+        if (length > Integer.MAX_VALUE) {
+
+        }
+
+        byte[] bytes = new byte[(int) length];
+
+        int offset = 0;
+        int numRead = 0;
+        while (offset < bytes.length
+                && (numRead = is.read(bytes, offset, bytes.length - offset)) >= 0) {
+            offset += numRead;
+        }
+
+        if (offset < bytes.length) {
+            throw new IOException();
+        }
+
+        is.close();
+        return bytes;
     }
 }
